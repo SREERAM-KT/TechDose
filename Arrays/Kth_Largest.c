de9be3887c826649a2df61a0f56f0e588852f7b2
SREERAM-KT <75211426+SREERAM-KT@users.noreply.github.com>
2021-12-28 20:08:45 +0530

    Kth Largest Element in Array

---
+++ Arrays/Kth_Largest.c
@@ -0,0 +1,12 @@
+int findKthLargest(int* nums, int numsSize, int k){
+for(int i=0;i<numsSize-1;i++){
+    for(int j=i+1;j<numsSize;j++){
+        if(nums[i]>nums[j]){
+        int t=nums[i];
+        nums[i]=nums[j];
+        nums[j]=t;
+        }
+    }
+}
+    return nums[numsSize-k];
+}
