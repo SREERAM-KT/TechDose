66ae0d7fb1d56e2881b331703e5b23af14b1718f
SREERAM-KT <75211426+SREERAM-KT@users.noreply.github.com>
2022-02-26 09:51:51 +0530

    Is Grpah Bipartite

---
+++ Graph/IsGraphBipartite.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int[] colors = new int[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (colors[i] == 0 && !hasEvenCycle(graph, colors, i, 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean hasEvenCycle(int[][] graph, int[] colors, int node, int c) {
+        if (colors[node] != 0) {
+            return (colors[node] == c);
+        }
+        colors[node] = c;
+        for (int i : graph[node]) {
+            if (!hasEvenCycle(graph, colors, i, -c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
