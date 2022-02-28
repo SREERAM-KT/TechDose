b6938ba8fa0838a7f3d1fb57a178ac44ab9cd9ab
SREERAM-KT <75211426+SREERAM-KT@users.noreply.github.com>
2022-02-28 10:17:03 +0530

    Course Schedule II updated

--- Graph/CourseSchedule_II.java
+++ Graph/CourseSchedule_II.java
@@ -1,61 +1,43 @@
 class Solution {
-    static int WHITE = 1;
-    static int GRAY = 2;
-    static int BLACK = 3;
-    boolean isPossible;
-    Map<Integer, Integer> color;
-    Map<Integer, List<Integer>> adjList;
-    List<Integer> topologicalOrder;
-
-    private void init(int numCourses) {
-        this.isPossible = true;
-        this.color = new HashMap<Integer, Integer>();
-        this.adjList = new HashMap<Integer, List<Integer>>();
-        this.topologicalOrder = new ArrayList<Integer>();
-        for (int i = 0; i < numCourses; i++) {
-            this.color.put(i, WHITE);
-        }
-    }
-
-    private void dfs(int node) {
-        if (!this.isPossible) {
-            return;
-        }
-        this.color.put(node, GRAY);
-        for (Integer neighbor : this.adjList.getOrDefault(node, new ArrayList<Integer>())) {
-            if (this.color.get(neighbor) == WHITE) {
-                this.dfs(neighbor);
-            } else if (this.color.get(neighbor) == GRAY) {
-                this.isPossible = false;
-            }
-        }
-        this.color.put(node, BLACK);
-        this.topologicalOrder.add(node);
-    }
-
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        this.init(numCourses);
-        for (int i = 0; i < prerequisites.length; i++) {
-            int dest = prerequisites[i][0];
-            int src = prerequisites[i][1];
-            List<Integer> lst = adjList.getOrDefault(src, new ArrayList<Integer>());
-            lst.add(dest);
-            adjList.put(src, lst);
+        List<List<Integer>> adj = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        boolean[] temp = new boolean[numCourses];
+        boolean[] perm = new boolean[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] i : prerequisites) {
+            adj.get(i[0]).add(i[1]);
         }
         for (int i = 0; i < numCourses; i++) {
-            if (this.color.get(i) == WHITE) {
-                this.dfs(i);
+            if (cycle(i, temp, perm, adj, list)) {
+                return new int[0];
             }
         }
-        int[] order;
-        if (this.isPossible) {
-            order = new int[numCourses];
-            for (int i = 0; i < numCourses; i++) {
-                order[i] = this.topologicalOrder.get(numCourses - i - 1);
-            }
-        } else {
-            order = new int[0];
+        int[] result = new int[list.size()];
+        for (int i = 0; !list.isEmpty(); i++) {
+            result[i] = list.remove(0);
         }
-        return order;
+        return result;
+    }
+
+    public boolean cycle(int u, boolean[] temp, boolean[] perm, List<List<Integer>> adj, List<Integer> list) {
+        if (temp[u]) {
+            return true;
+        }
+        if (perm[u]) {
+            return false;
+        }
+        temp[u] = true;
+        for (int v : adj.get(u)) {
+            if (cycle(v, temp, perm, adj, list)) {
+                return true;
+            }
+        }
+        list.add(u);
+        perm[u] = true;
+        temp[u] = false;
+        return false;
     }
 }
